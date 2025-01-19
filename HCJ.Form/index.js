841d80a625e5535e73140612e14d5d965c4f4d26
Magi112233 <mageswarimagi07@gmail.com>
2025-01-19 05:26:18 -0800

    as

--- HCJ.Form/index.js
+++ HCJ.Form/index.js
@@ -1,60 +1,72 @@
-const form = document.querySelector('#form');
-const username = document.querySelector('#usernsme');
-const email = document.querySelector('#email');
-const password =document.querySelector('#password');
-const cpassword = document.querySelector('#cpassword');
+const form = document.querySelector("#form");
+const username = document.querySelector("#usernsme");
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
+const cpassword = document.querySelector("#cpassword");
 
-form.addEventListener('submit',(e)=>){
-    e.preventDefault();    //error
-    validateInputs();     //
+form.addEventListener("submit", (e) => {
+  e.preventDefault(); //error
+  validateInputs(); //
+});
+
+function validateInputs() {
+  const usernameVal = username.value.trim(); //enptyspace
+  const emailVal = email.value.trim();
+  const passwordVal = password.value.trim();
+  const cpasswordVal = cpassword.value.trim();
+
+  if (usernameVal === "") {
+    setError(username, "username is required");
+  } else {
+    setSuccess(username);
+  }
+  //------------
+  if (emailVal === "") {
+    setError(email, "Email is required");
+  } else if (!validateEmail(emailVal)) {
+    setError(email, "please enter a valid email");
+  } else {
+    setSuccess(email);
+  }
+  //-------------
+  if (passwordVal === "") {
+    setError(password, "password is required");
+  } else if (passwordVal.length < 8) {
+    setError(password, "password must be atleast 8 value");
+  } else {
+    setSuccess(password);
+  }
+  //------------
+  if (cpassword === "") {
+    setError(cpassword, "confirm password is required");
+  } else if (cpasswordVal !== password) {
+    setError(cpassword, "password does not match");
+  } else {
+    setSuccess(cpassword);
+  }
 }
 
-function validateInputs(){
-    const usernameVal = username.value.trim() ;  //enptyspace
-    const emailVal= email.value.trim();
-    const passwordVal = password.value.trim();
-    const cpasswordVal = cpassword.value.trim();
-
-    if(usernameVal)===''{
-        setError(username,'username is required')
-    }else{
-        setSuccess(username)        
-    }
-
-    if(email)===''{
-        setError(email.'')
-    }
-
-    
-    
-
-
-}
 //element - password,msg-pas is reqd
-function setError(element,message){
-    const inputGroup = element.parentElement;
-    const errorElement = inputGroup.querySelector('error')
+function setError(element, message) {
+  const inputGroup = element.parentElement;
+  const errorElement = inputGroup.querySelector("error");
 
-    errorElement.innerText = message;
-    inputGroup.classList.add('error')
-    inputGroup.classList.remove('success')
-
+  errorElement.innerText = message;
+  inputGroup.classList.add("error");
+  inputGroup.classList.remove("success");
 }
 
-function setSuccess(element){
-    const inputGroup = element.parentElement;
-    const errorElement = element.querySelector('error')
+function setSuccess(element) {
+  const inputGroup = element.parentElement;
+  const errorElement = element.querySelector("error");
 
-    errorElement.innerText = '';
-    inputGroup.classList.add('success')
-    inputGroup.classList.remove('error')
+  errorElement.innerText = "";
+  inputGroup.classList.add("success");
+  inputGroup.classList.remove("error");
 }
 
-const validateEmail = (email)=>{
-    return String (email)
+const validateEmail = (email) => {
+  return String(email)
     .toLowerCase()
-    .match(
-    /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
-    );
+    .match(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/);
 };
-    
