ce7f8995f97615f2ea232c3ea80d9159c7453458
Magi112233 <mageswarimagi07@gmail.com>
2025-03-04 21:43:14 -0800

    t

---
+++ jsclass/coltrolflow.js
@@ -0,0 +1,17 @@
+// if, else, else if, switch, breack
+
+// const age = "25";
+
+// if (age < 20) {
+//   console.log("haii");
+// } else if (age < 70) {
+//   console.log("hello");
+// } else {
+//   console.log("hello world");
+// }
+
+// switch
+const lia_father_name = "Ayyanar";
+
+switch (lia_father_name) {
+}
---
+++ jsclass/loopss.js
@@ -0,0 +1,24 @@
+let words = ["one", "two", "three", "four", "five"];
+
+// for (let i = 0; i <= 5; i++) {
+//   console.log words [i]);
+// }
+
+// for (let i = 0; i < words .lenght; i++) {
+//   console.log(nmu[i]);
+// }
+// console.log words [1]);
+
+// for (let i of words) {
+//   console.log(words[i]);
+// }
+
+let person = {
+  name: "magi",
+  age: "21",
+  hname: "ayyanar",
+  fname: "dhandabani",
+};
+for (let i in person) {
+  console.log(person[i]);
+}
